b4e4475fd91475f96305e9378908b0289c30f734
avinash13jun <avinash13jun@gmail.com>
2015-06-27 01:20:41 +0530

    defined new_dead_cell and new_alive_cell and initialized variable state to get the their status to be alive or dead. using the terinary operator if the state is alive it goes to live_cell_generation_rule or else dead_cell_generation rule. In live cell rule the cell is alive if the neighbours are 2,3 or else it is dead. In dead cell rule if the neighbours are exactly 3, the cell is alive or else dead

--- lib/game_of_life/cell.rb
+++ lib/game_of_life/cell.rb
@@ -3,29 +3,41 @@ class GameOfLife::Cell
   DEAD = "dead"
 
   def self.new_dead_cell
-  	new(DEAD)
-  	
+    new(DEAD)
+    
   end
   def self.new_live_cell
-  	new(ALIVE)
+    new(ALIVE)
   end
-  def initialize state
-  	@state = state
-  end
-  attr_reader :state
-  def dead?
-
-  	state == DEAD
-  end
-  def alive?
-  	state == ALIVE
+  def initialize(state)
+    @state = state
   end
   
-  def self.next_generation ()
-    
-  build_neighbours == i;
-    
+  def dead?
+
+    @state == DEAD
   end
 
+  def alive?
+    @state == ALIVE
+  end
+  
+  def next_generation(neighbours)
+    @neighbours = neighbours
+    @state = alive? ? live_cell_generation_rule : dead_cell_generation_rule
+    self
+  end
+    
+  def live_cell_generation_rule
+    [2,3].include?(number_of_alive_cells(@neighbours)) ? ALIVE : DEAD
+  end
+
+  def dead_cell_generation_rule
+    [3].include?(number_of_alive_cells(@neighbours)) ? ALIVE : DEAD
+  end
+
+  def number_of_alive_cells(array)
+    array.select{ |cell| cell.alive? }.length
+  end
   
 end
